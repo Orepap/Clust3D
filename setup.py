e5b5556d0bd1d6ed01d9d14d0abcd8ccdd9d65c9
Orestis Papagiannopoulos <93657525+Orepap@users.noreply.github.com>
2023-09-26 11:46:48 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ setup(
     version='1.0',
     packages=find_packages(),
     author_email="orepap@uoi.gr",
-    install_requires=["scikit-learn >= 1.0.2",
-                      "numpy >= 1.21.6",
-                      "pandas >= 1.4.0",
-                      "matplotlib >= 3.5.1"],
     description='Timeseries Multi-Dimensional Clustering of Gene Expression Data from Systemic Autoinflammatory Diseases'
 )
 
