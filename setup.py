ec4f326ade00ff60cbfc13cee3e75aa966a3616a
Orestis Papagiannopoulos <93657525+Orepap@users.noreply.github.com>
2024-02-27 11:09:53 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Clust3D',
+    version='1.0',
+    packages=find_packages(),
+    author_email="orepap@uoi.gr",
+    description='Clust3D - 3D Clustering tool'
+)
+
