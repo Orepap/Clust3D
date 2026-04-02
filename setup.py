79bbc774a915c156343ef21ca3696c2241689504
Orestis Papagiannopoulos <93657525+Orepap@users.noreply.github.com>
2026-04-02 15:47:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Clust3D',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     author_email="orepap@uoi.gr",
     description='Clust3D - 3D Clustering tool'
